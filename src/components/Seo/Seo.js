bae226d627a150883695ca536c87179446f97e98
Yaser Nicaragua <yanv1991@gmail.com>
2020-06-08 15:14:08 -0400

    fix og image

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -9,6 +9,14 @@ const Seo = props => {
   const postDescription = ((data || {}).frontmatter || {}).description;
   // const postCover = ((data || {}).frontmatter || {}).cover;
   const postSlug = ((data || {}).fields || {}).slug || "";
+  const category = data && data.frontmatter && data.frontmatter.category;
+  const contentImage =
+    data &&
+    data.frontmatter &&
+    data.frontmatter.cover &&
+    data.frontmatter.cover.childImageSharp &&
+    data.frontmatter.cover.childImageSharp.resize &&
+    data.frontmatter.cover.childImageSharp.resize.src;
 
   const title = postTitle ? `${postTitle} - ${config.shortSiteTitle}` : config.siteTitle;
   const description = postDescription ? postDescription : config.siteDescription;
@@ -28,10 +36,10 @@ const Seo = props => {
       {/* OpenGraph tags */}
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="og:image:width" content="524" />
-      <meta property="og:image:height" content="274" />
+      <meta property="og:description" content={category || description} />
+      <meta property="og:image" content={contentImage || image} />
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="147" />
       <meta property="og:type" content="website" />
       <meta property="fb:app_id" content={facebook.appId} />
       {/* Twitter Card tags */}
