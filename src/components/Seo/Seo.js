ee96df82373130d677eabde9d2c08ded841f5ca3
Yaser Nicaragua <yanv1991@gmail.com>
2020-06-08 15:35:32 -0400

    fix og image

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -37,7 +37,7 @@ const Seo = props => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={category || description} />
-      <meta property="og:image" content={contentImage || image} />
+      <meta property="og:image" content={`https://laasistentecr.com${contentImage || image}`} />
       <meta property="og:image:width" content="300" />
       <meta property="og:image:height" content="147" />
       <meta property="og:type" content="website" />
